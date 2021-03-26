890cd1c044a315dc74057c68bece796193b2738c
GayanKod <gayan.lakshan9@gmail.com>
2021-03-27 00:54:03 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
 import Welcome from "./components/pages/welcome/welcome";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <StrictMode>
-    <Welcome />
-  </StrictMode>,
-  rootElement
+    <Welcome />,
+    rootElement
 );
