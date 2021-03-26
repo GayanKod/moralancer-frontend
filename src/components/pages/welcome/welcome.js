efd1ca15dc8f52f8378fc436f60325bf9c28f12a
GayanKod <gayan.lakshan9@gmail.com>
2021-03-26 17:23:33 +0530

    Update the folder structure of moralancer

---
+++ src/components/pages/welcome/welcome.js
@@ -0,0 +1,10 @@
+import "./welcome.css"
+
+export default function Welcome() {
+    return (
+        <div className="Welcome">
+          <h1>Hello Team Hug JS</h1>
+          <h2>Start editing to see some magic happen!</h2>
+        </div>
+      );
+}
