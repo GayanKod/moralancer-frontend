780e6eb1d5006ccf527e00abe1ed6e3d238286f2
Dushani Ranasinghe <uththaradushani@gmail.com>
2021-03-27 16:29:57 +0530

    Created Sub Navbar and added it to 'About us' page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from './components/common/navbar';
 import{BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import welome from './components/pages/welcome/welcome'
 import Footer from './components/common/footer'
+import AboutUs from './components/pages/aboutus/Aboutus'
+
 function App() {
     return (
       <>
@@ -10,6 +12,7 @@ function App() {
         <Navbar/>
         <Switch>
         <Route path='/' exact component={welome}/>
+        <Route path='/aboutus' exact component={AboutUs}/>
       
       </Switch>
       <Footer/>
