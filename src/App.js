0edfca1995ded1fb8cf8df9d7e1e13ad676ecd9f
Dushani Ranasinghe <uththaradushani@gmail.com>
2021-03-27 14:53:31 +0530

    Created footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/common/navbar';
 import{BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import welome from './components/pages/welcome/welcome'
+import Footer from './components/common/footer'
 function App() {
     return (
       <>
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' exact component={welome}/>
       
       </Switch>
+      <Footer/>
         </Router>
         </>
     );
---
+++ src/components/common/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.css';
+
+function Footer() {
+    return (
+        <div className="footer-container">
+            <h6> <i className="fas fa-copyright"/> 2021 - TEAM HUGJS (Batch 19)</h6>
+            <h5>|All Rights Reserved|</h5>
+            <h5>University of Moratuwa - FACULTY OF IT</h5>
+           
+        </div>
+    )
+}
+
+export default Footer
