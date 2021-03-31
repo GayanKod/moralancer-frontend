bda7653f19bc431256af648208f9c03b68f1fb39
GayanKod <gayan.lakshan9@gmail.com>
2021-04-01 02:15:01 +0530

    Get data from MongoDB and display gigs

--- src/components/GigCard.js
+++ src/components/GigCard.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-function GigCard(props) {
+function GigCard({props}) {
     return(
         <Link className='gigcards__item__link' to={`Gigs/${props.id}`}>
-        <div className="Gig-container">
-            <img className="gigimg" src={props.img} alt="" />
-            <h4 className="label">{props.label}</h4>
-            <h3 className="title">{props.gigTitle}</h3>
-            <h4 className="seller">By {props.seller}</h4>
-            <h4 className="price">{props.basicPrice}</h4>
-            <h4 className="rate"><i class="fa fa-star" aria-hidden="true"></i> {props.starRate}</h4>
-        </div>
+        
+            {props.map((gig,key)=>(
+                <div className="Gig-container">
+                <img className="gigimg" src={gig.img} alt="" />
+                <h4 className="label">{gig.gigCategory}</h4>
+                <h3 className="title">{gig.gigTitle}</h3>
+                <h4 className="seller">By {gig.seller}</h4>
+                <h4 className="price">Rs. {gig.gigBasicPrice}</h4>
+                <h4 className="rate"><i class="fa fa-star" aria-hidden="true"></i> {gig.starRate}</h4>
+                </div>
+            ))}
+        
         </Link>
+        
     );
 }
+
+
 
 export default GigCard;
