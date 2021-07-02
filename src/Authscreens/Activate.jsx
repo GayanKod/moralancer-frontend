c2a284bad6dd9f3cf29df9d7e1430a257aaade32
GayanKod <gayan.lakshan9@gmail.com>
2021-07-02 14:43:24 +0530

    styling changes

--- src/Authscreens/Activate.jsx
+++ src/Authscreens/Activate.jsx
@@ -49,7 +49,7 @@ const Activate = ({ match }) => {
   };
 
   return (
-    <div className='min-h-screen bg-gray-100 text-gray-900 flex justify-center'>
+    <div className='min-h-screen bg-red-100 text-red-900 flex justify-center'>
       {isAuth() ? <Redirect to='/' /> : null}
       <ToastContainer />
       <div className='max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1'>
@@ -60,38 +60,38 @@ const Activate = ({ match }) => {
             </h1>
 
             <form
-              className='w-full flex-1 mt-8 text-indigo-500'
+              className='w-full flex-1 mt-8 text-red-500'
               onSubmit={handleSubmit}
             >
               <div className='mx-auto max-w-xs relative '>
                 <button
                   type='submit'
-                  className='mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none'
+                  className='mt-5 tracking-wide font-semibold bg-red-500 text-red-100 w-full py-4 rounded-lg hover:bg-red-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none'
                 >
                   <i className='fas fa-user-plus fa 1x w-6  -ml-2' />
                   <span className='ml-3'>Activate your Account</span>
                 </button>
               </div>
               <div className='my-12 border-b text-center'>
-                <div className='leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2'>
+                <div className='leading-none px-2 inline-block text-sm text-red-600 tracking-wide font-medium bg-white transform translate-y-1/2'>
                   Or sign up again
                 </div>
               </div>
               <div className='flex flex-col items-center'>
                 <a
                   className='w-full max-w-xs font-bold shadow-sm rounded-lg py-3
-           bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5'
+           bg-red-100 text-red-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5'
                   href='/register'
                   target='_self'
                 >
-                  <i className='fas fa-sign-in-alt fa 1x w-6  -ml-2 text-indigo-500' />
+                  <i className='fas fa-sign-in-alt fa 1x w-6  -ml-2 text-red-500' />
                   <span className='ml-4'>Sign Up</span>
                 </a>
               </div>
             </form>
           </div>
         </div>
-        <div className='flex-1 bg-indigo-100 text-center hidden lg:flex'>
+        <div className='flex-1 bg-red-100 text-center hidden lg:flex'>
           <div
             className='m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat'
             style={{ backgroundImage: `url(${authSvg})` }}
--- src/Authscreens/BuyerRegister.jsx
+++ src/Authscreens/BuyerRegister.jsx
@@ -7,7 +7,7 @@ import { Link, Redirect } from 'react-router-dom';
 require("dotenv").config();
 const dotenv = require("dotenv");
 
-const Register = () => {
+const BuyerRegister = () => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -64,7 +64,7 @@ const Register = () => {
   };
 
   return (
-    <div className='min-h-screen bg-gray-100 text-gray-900 flex justify-center'>
+    <div className='min-h-screen bg-red-100 text-red-600 flex justify-center'>
       {isAuth() ? <Redirect to='/' /> : null}
       <ToastContainer />
       <div className='max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1'>
@@ -75,33 +75,33 @@ const Register = () => {
             </h1>
 
             <form
-              className='w-full flex-1 mt-8 text-indigo-500'
+              className='w-full flex-1 mt-8 text-red-500'
               onSubmit={handleSubmit}
             >
               <div className='mx-auto max-w-xs relative '>
                 <input
-                  className='w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white'
+                  className='w-full px-8 py-4 rounded-lg font-medium bg-red-100 border border-red-200 placeholder-red-500 text-sm focus:outline-none focus:border-red-400 focus:bg-white'
                   type='text'
                   placeholder='Name'
                   onChange={handleChange('name')}
                   value={name}
                 />
                 <input
-                  className='w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5'
+                  className='w-full px-8 py-4 rounded-lg font-medium bg-red-100 border border-red-200 placeholder-red-500 text-sm focus:outline-none focus:border-red-400 focus:bg-white mt-5'
                   type='email'
                   placeholder='Email'
                   onChange={handleChange('email')}
                   value={email}
                 />
                 <input
-                  className='w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5'
+                  className='w-full px-8 py-4 rounded-lg font-medium bg-red-100 border border-red-200 placeholder-red-500 text-sm focus:outline-none focus:border-red-400 focus:bg-white mt-5'
                   type='password'
                   placeholder='Password'
                   onChange={handleChange('password1')}
                   value={password1}
                 />
                 <input
-                  className='w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5'
+                  className='w-full px-8 py-4 rounded-lg font-medium bg-red-100 border border-red-200 placeholder-red-500 text-sm focus:outline-none focus:border-red-400 focus:bg-white mt-5'
                   type='password'
                   placeholder='Confirm Password'
                   onChange={handleChange('password2')}
@@ -109,32 +109,32 @@ const Register = () => {
                 />
                 <button
                   type='submit'
-                  className='mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none'
+                  className='mt-5 tracking-wide font-semibold bg-red-500 text-red-100 w-full py-4 rounded-lg hover:bg-red-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none'
                 >
                   <i className='fas fa-user-plus fa 1x w-6  -ml-2' />
                   <span className='ml-3'>{textChange}</span>
                 </button>
               </div>
               <div className='my-12 border-b text-center'>
-                <div className='leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2'>
+                <div className='leading-none px-2 inline-block text-sm text-red-600 tracking-wide font-medium bg-white transform translate-y-1/2'>
                   Or sign with email or social login
                 </div>
               </div>
               <div className='flex flex-col items-center'>
                 <a
                   className='w-full max-w-xs font-bold shadow-sm rounded-lg py-3
-           bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5'
+           bg-red-100 text-red-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5'
                   href='/SignIn'
                   target='_self'
                 >
-                  <i className='fas fa-sign-in-alt fa 1x w-6  -ml-2 text-indigo-500' />
-                  <span className='ml-4'>Sign In</span>
+                  <i className='fas fa-sign-in-alt fa 1x w-6  -ml-2 text-red-500' />
+                  <span className='ml-4 text-red-500'>Sign In</span>
                 </a>
               </div>
             </form>
           </div>
         </div>
-        <div className='flex-1 bg-indigo-100 text-center hidden lg:flex'>
+        <div className='flex-1 bg-red-100 text-center hidden lg:flex'>
           <div
             className='m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat'
             style={{ backgroundImage: `url(${authSvg})` }}
@@ -146,4 +146,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default BuyerRegister;
